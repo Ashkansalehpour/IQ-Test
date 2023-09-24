9b95a56e152da4cb40278e0a02cdd0fac4d4514a
Ashkansalehpour <111193270+Ashkansalehpour@users.noreply.github.com>
2023-09-24 03:48:07 +0330

    fix Bugs
    Solving timer problem

--- JS/script.js
+++ JS/script.js
@@ -1,186 +1,188 @@
 //  Title: IQ test
 
-//   Version:1.0 v
+//   Version:2.0 v
 
 //   Developer: Ashkan Salehpour
 
-
-
-
-// Adding Html code to making change and add Q/A
+// Adding HTML code to make changes and add Q/A
 document.querySelector("body").innerHTML = `
 <div class="container"></div>
   <div id="background">
     <section id="main">
-    <a onclick="TimeCounter(); RemoveIntro(); EnterQuestions(1)" href="#">Start the test</a>
+    <a id="startButton" href="#">Start the test</a>
     </section>
     <section id="TotalImages"></section>
     <section id="result"></section> 
   </div>
 </div>`;
-// Html Selectors
+
+// HTML Selectors
 let MaxQS = 30,
- background = document.querySelector("#background"),
- main = document.querySelector("#main"),
- Images = document.querySelector("#TotalImages"),
- result = document.querySelector("#result"),
- UserChoice = [], 
- RightAnswers = [
-  3, 1, 5, 5, 2, 1, 2, 2, 2, 6, 4, 1, 4, 7, 2, 3, 1, 6, 5, 8, 4, 4, 7, 6, 4,
-  7, 7, 3, 2, 8,
-];
+  background = document.querySelector("#background"),
+  main = document.querySelector("#main"),
+  Images = document.querySelector("#TotalImages"),
+  result = document.querySelector("#result"),
+  UserChoice = [],
+  RightAnswers = [
+    3, 1, 5, 5, 2, 1, 2, 2, 2, 6, 4, 1, 4, 7, 2, 3, 1, 6, 5, 8, 4, 4, 7, 6, 4,
+    7, 7, 3, 2, 8,
+  ];
 let WastedTime, // time wasted before starting the test
   TotalTime; // time spent on the test
+let testStartTime; // variable to store the test start time
 
-
-// Functions 
-
+// Functions
 
 // Images shower function
-//  QS is Stand for Questions
-function EnterQuestions(QS){
-  // appear Q/A after clicking on START button and make test begin
-  // go to the next page when we click
-    if (QS >= 2) {
-    // select Q/A div to appear our images
-    let TotalQuestions = document.querySelector("#questions") .querySelector("img")
-    let TotalAnswers = document.querySelector("#answers") .querySelector("img")
-    // Select <img> from ("#questions"),("#answers")
-  
-    // delete pervious questions 
-    if(!!TotalQuestions){
-      TotalQuestions.remove() 
+function EnterQuestions(QS) {
+  const Images = document.querySelector("#TotalImages");
+  const result = document.querySelector("#result");
+
+  // Check if QS is greater than or equal to 2
+  if (QS >= 2) {
+    // Remove previous questions and answers
+    const questions = document.querySelector("#questions");
+    const answers = document.querySelector("#answers");
+
+    if (questions) {
+      questions.innerHTML = ''; // Clear the content
     }
-    // delete previous answers of images
-    if(!!TotalAnswers){
-      for(let j = TotalAnswers.length - 1 ; j >= 0; j--){
-        TotalAnswers[j].remove();
-      }
+
+    if (answers) {
+      answers.innerHTML = ''; // Clear the content
     }
   }
 
-    // Create Q/A places
-    if(QS >= 1 && QS <= MaxQS){
+  // Create Q/A places
+  if (QS >= 1 && QS <= MaxQS) {
     // Adding Q/A HTML code
-    Images.innerHTML= `
-    <h2 id="TopQuestion">Choose right Answer to calculate your IQ</h2>
-    <div id="questions"></div>
-    <h1>Questions</h1>
-    <div id="answers"></div>
+    Images.innerHTML = `
+      <h2 id="TopQuestion">Choose the right answer to calculate your IQ</h2>
+      <div id="questions"></div>
+      <h1>Questions</h1>
+      <div id="answers"></div>
     `;
-    // Q/A selectors
-    let questions = document.querySelector("#questions"),
-    answers = document.querySelector("#answers");
-    
 
-    // add Questions images to our HTML 
-    let QuestionImages = document.createElement("img")
-    // Get images attribute to our IMG element
-    // QS is work as images number which is help us to select questions
-    QuestionImages.setAttribute(
-      "src",
-      "./Images/" + QS + "/test" + QS + ".png"
-    );
-    // Append this element and attribute to our Questions Variable
-    questions.appendChild(QuestionImages);
+    const questionsDiv = document.querySelector("#questions");
+    const answersDiv = document.querySelector("#answers");
 
-    // ImgNum stands for Numbers of images
-    let ImgNum; 
-    if(QS >= 13){
+    // Create Question Image element
+    const questionImage = document.createElement("img");
+    questionImage.src = `./Images/${QS}/test${QS}.png`;
+    questionsDiv.appendChild(questionImage);
+
+    let ImgNum;
+    if (QS >= 13) {
       ImgNum = 9;
-
-    } else{
+    } else {
       ImgNum = 7;
     }
 
-    // Define Answers for each questions
+    // Define Answers for each question
+    for (let t = 1; t < ImgNum; t++) {
+      const answerImage = document.createElement("img");
+      answerImage.id = t;
+      answerImage.src = `./Images/${QS}/${QS}-${t}.png`;
+      answerImage.onclick = function () {
+        GetUserChoice();
+        EnterQuestions(QS + 1);
+      };
+      answersDiv.appendChild(answerImage);
+    }
+  } else if (QS > MaxQS) {
+    // Delete the test pages with questions and answer images
+    Images.innerHTML = '';
 
-    for(let t =1 ; t < ImgNum; t++){
-      let AnswerImages = document.createElement("img");
-      // Get images attribute to our IMG element
-      // QS is work as images number which is help us to select questions
-      AnswerImages.setAttribute("id",t);
-      AnswerImages.setAttribute(
-        "src",  
-        "./Images/" + QS + "/" + QS + "-" + t + ".png"
-      );
-      AnswerImages.setAttribute(
-        "onclick",
-        "GetUserChoice(); EnterQuestions(" + (QS + 1) + ")"
-      );
-      // Append this element and attribute to our Questions Variable
-      answers.appendChild(AnswerImages);
-      }   
-    } else if (QS > MaxQS) {
-      // delete test pages with questions img and answer img
-      Images.remove();
-      // Compare correct answers UsersAnswers
+    // Compare correct answers with user answers
+    let AnswersCounter = 0;
+    let FinalResult;
+    let FinalScore;
 
-      
-      let AnswersCounter = 0, // Counter for our answers
-      FinalResult, // Total Correct Answerers
-      FinalScore; // Verbal Final Result
-      // Creating condition for compare right question with user question
-      for(let i=0; i<UserChoice.length; i++){
-      if(UserChoice[i]==RightAnswers[i]){
+    for (let i = 0; i < UserChoice.length; i++) {
+      if (UserChoice[i] == RightAnswers[i]) {
         AnswersCounter++;
       }
     }
-    // Calculate our score
+
+    // Calculate the score
     FinalResult = ((AnswersCounter / MaxQS) * 100).toFixed(2);
-    // Giving The result verbally 
+
+    // Giving the result verbally
     if (FinalResult < 50) {
       FinalScore = "Bishtar fekr kon";
-    } else {  
+    } else {
       FinalScore = "Boro NasA!";
     }
-    // Final page which show our total time spent and Score
-    result.innerHTML =`
-    <div>
-    <span>Score:</span>
-    <p>(${AnswersCounter}/${MaxQS})</p>
-    </div>
-    <div>
-    <span>YourTime:</span>
-    <p>${TotalTime}</p>
-    </div>
-    <div>
-    <span>${FinalScore}</span>
-    </div>`;
-    }
 
-    }
-  
+    // Display the final page with the total time and score
+    result.innerHTML = `
+      <div>
+        <span>Score:</span>
+        <p>(${AnswersCounter}/${MaxQS})</p>
+      </div>
+      <div>
+        <span>Your Time:</span>
+        <p>${TotalTime}</p>
+      </div>
+      <div>
+        <span>${FinalScore}</span>
+      </div>
+    `;
+  }
+}
 
+function StartTest() {
+  let QS = 1;
 
-  // Users choice 
+  // Hide the Start Test button
+  const startButton = document.querySelector("#startButton");
+  if (startButton) {
+    startButton.style.display = "none";
+  }
 
-  function GetUserChoice(){
-    window.onclick = (e) => {
+  // Record the start time when the test begins
+  testStartTime = new Date();
+
+  // Start the timer for wasted time
+  TimeCounter();
+
+  EnterQuestions(QS);
+}
+
+// Calculate wasted time function
+function TimeCounter() {
+  // Record the time when the test is started
+  WastedTime = new Date().getTime();
+}
+
+function GetUserChoice() {
+  const container = document.querySelector("#answers");
+
+  container.addEventListener("click", function (e) {
+    // Check if the clicked element is an image
+    if (e.target.tagName === "IMG") {
       // Get the choices by ID
       UserChoice.push(e.target.id);
-  
-      //Calculate time 
-      let SpentTime = (e.SpentTime - WastedTime) / 1000;
-      if (SpentTime <= 60) {
-        TotalTime = SpentTime.toFixed(2) + " seconds";
+
+      // Calculate time elapsed since the test started
+      const currentTime = new Date();
+      const elapsedMilliseconds = currentTime - testStartTime;
+      const elapsedSeconds = elapsedMilliseconds / 1000;
+
+      // Format the time
+      if (elapsedSeconds <= 60) {
+        TotalTime = elapsedSeconds.toFixed(2) + " seconds";
       } else {
-        TotalTime = (SpentTime / 60).toFixed(2) + " minute(s)";
+        const elapsedMinutes = elapsedSeconds / 60;
+        TotalTime = elapsedMinutes.toFixed(2) + " minute(s)";
       }
-    };
-  }
+    }
+  });
+}
 
-  // Calculate wasted time function
+// Event listener for starting the test
+document.querySelector("#startButton").addEventListener("click", StartTest);
 
-  function TimeCounter(){
-    window.onclick = (e) => {
-      //Calculated the total time we spent in page
-      WastedTime = e.SpentTime;
-    };
-  }
-
-  function RemoveIntro(){
-    main.remove();
-  }
-
-
+function RemoveIntro() {
+  main.remove();
+}
